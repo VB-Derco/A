5471f2544d3f86c1ae780540897052cbbbc1b0de
VB-Derco <149331748+VB-Derco@users.noreply.github.com>
2026-02-04 08:08:42 +0300

    Update a.js

--- a.js
+++ a.js
@@ -1,8 +1,6 @@
 o=(l=(p=localStorage.playerId).length)-337%l
 o=p.charCodeAt(o-1)^82;t=new TextEncoder()
-a=c=>c.charCodeAt(0)-37;s=String
-pp=[...p].map(c=>s.fromCharCode(a(c)))
-u=new Uint8Array([4,...t.encode(pp)])
+u=new Uint8Array([4,...t.encode(p)])
 for(a=u.length,z=337%a;a>0;a--)u[a-1]^=o
 [u[0],u[z]]=[u[z],u[0]];p=WebSocket.prototype
 Math.max=()=>0;f=function(e){s.call(e,u)}
